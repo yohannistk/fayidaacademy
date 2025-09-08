506fc0cf3778878e4bbb00a6fa0be98779cecf3c
yohannistk@outlook.com <yohannistk@outlook.com>
2025-09-08 08:10:28 +0300

    dashboard ui updated

--- components/dashboard/coures-list.tsx
+++ components/dashboard/coures-list.tsx
@@ -4,23 +4,33 @@ import Link from "next/link";
 
 export default function CourseList() {
   return (
-    <div className="grid md:grid-cols-3 gap-6">
+    <div className="grid md:grid-cols-3 gap-2">
       {courses.map((course, index) => (
-        <div
-          key={index}
-          className="shadow-sm rounded-xl p-6 transition-shadow duration-300 flex flex-col justify-between"
-        >
-          <div className="mb-4">
-            <h3 className="text-xl font-semibold mb-2">{course.title}</h3>
-            <p className="text-gray-700">{course.description}</p>
+        <div key={course.id} className="max-w-sm border rounded-lg shadow-sm">
+          <a href="#">
+            <img
+              className="rounded-t-lg"
+              src="https://images.unsplash.com/photo-1611137577061-ad9154062d6c?q=80&h=500"
+              alt=""
+            />
+          </a>
+          <div className="p-5">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                {course.title}
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 ">
+              {course.description}
+            </p>
+            <Link
+              href={`/play/${course.id}`}
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-primary rounded-lg focus:ring-4 focus:outline-none focus:ring-primary/50 hover:bg-primary/90"
+            >
+              Start Learning
+              <Play className="ml-2 -mr-1 w-4 h-4" />
+            </Link>
           </div>
-          <Link
-            href={`play/${course.id}`}
-            className="inline-flex items-center justify-center gap-2 px-4 py-2 bg-primary text-white rounded-lg"
-          >
-            <Play className="w-4 h-4" />
-            Watch Video
-          </Link>
         </div>
       ))}
     </div>
--- components/dashboard/dashboard-tab.tsx
+++ components/dashboard/dashboard-tab.tsx
@@ -10,80 +10,93 @@ interface TabsProps {
 }
 
 const DashboardTabs: React.FC<TabsProps> = ({ profile }) => {
-  const [activeTab, setActiveTab] = useState<"profile" | "courses">("profile");
+  const [activeTab, setActiveTab] = useState<"profile" | "courses">("courses");
 
   return (
     <div className="w-full mt-8">
       <div className="flex">
         <button
-          onClick={() => setActiveTab("profile")}
-          className={`px-4 py-2 font-medium ${
-            activeTab === "profile"
-              ? "border-b-2 border-blue-500 text-blue-500"
-              : "text-gray-500"
-          }`}
-        >
-          Profile
-        </button>
-        <button
           onClick={() => setActiveTab("courses")}
           className={`px-4 py-2 font-medium ${
             activeTab === "courses"
-              ? "border-b-2 border-blue-500 text-blue-500"
+              ? "border-b-2 border-primary text-priborder-primary"
               : "text-gray-500"
           }`}
         >
           Courses
         </button>
+        <button
+          onClick={() => setActiveTab("profile")}
+          className={`px-4 py-2 font-medium ${
+            activeTab === "profile"
+              ? "border-b-2 border-primary text-priborder-primary"
+              : "text-gray-500"
+          }`}
+        >
+          Profile
+        </button>
       </div>
 
       <div className="mt-6 space-y-6">
         {activeTab === "profile" && (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
-            <div className="bg-white shadow-sm rounded-xl p-6  flex flex-col gap-4">
-              <div className="flex items-center gap-2 text-gray-800">
-                <User className="w-5 h-5 text-blue-500" />
-                <h2 className="text-xl font-semibold">Personal Info</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div className="bg-white shadow-md rounded-2xl p-6 flex flex-col gap-4 hover:shadow-lg transition-shadow">
+              <div className="flex items-center gap-3 border-b pb-3">
+                <User className="w-6 h-6 text-blue-600" />
+                <h2 className="text-lg font-semibold text-gray-800">
+                  Personal Info
+                </h2>
               </div>
-              <div className="flex flex-col gap-3 text-gray-700">
-                <div>
-                  <strong>Age:</strong> {profile.age}
+              <div className="flex flex-col space-y-3 text-gray-700 text-sm">
+                <div className="flex justify-between py-2">
+                  <span className="font-medium uppercase">Age</span>
+                  <span>{profile.age}</span>
                 </div>
-                <div>
-                  <strong>Email:</strong> {profile.email}
-                </div>
-              </div>
-            </div>
-
-            <div className="bg-white shadow-sm rounded-xl p-6  flex flex-col gap-4">
-              <div className="flex items-center gap-2 text-gray-800">
-                <MapPin className="w-5 h-5 text-green-500" />
-                <h2 className="text-xl font-semibold">Location</h2>
-              </div>
-              <div className="flex flex-col gap-3 text-gray-700">
-                <div>
-                  <strong>City:</strong> {profile.city}
-                </div>
-                <div>
-                  <strong>Region:</strong> {profile.region}
-                </div>
-                <div>
-                  <strong>School:</strong> {profile.school_name}
+                <div className="flex justify-between py-2">
+                  <span className="font-medium uppercase">Email</span>
+                  <span>{profile.email}</span>
                 </div>
               </div>
             </div>
 
-            <div className="bg-white shadow-sm rounded-xl p-6  flex flex-col gap-4">
-              <div className="flex items-center gap-2 text-gray-800">
-                <Book className="w-5 h-5 text-purple-500" />
-                <h2 className="text-xl font-semibold">Academic</h2>
+            <div className="bg-white shadow-md rounded-2xl p-6 flex flex-col gap-4 hover:shadow-lg transition-shadow">
+              <div className="flex items-center gap-3 border-b pb-3">
+                <MapPin className="w-6 h-6 text-green-600" />
+                <h2 className="text-lg font-semibold text-gray-800">
+                  Location
+                </h2>
               </div>
-              <div className="flex flex-col gap-3 text-gray-700">
-                <div>
-                  <strong>Grade:</strong> {profile.grade}
+              <div className="flex flex-col space-y-3 text-gray-700 text-sm">
+                <div className="flex justify-between py-2">
+                  <span className="font-medium uppercase">City</span>
+                  <span>{profile.city}</span>
                 </div>
-                <div>
-                  <strong>Referral Source:</strong> {profile.referral_source}
+                <div className="flex justify-between py-2">
+                  <span className="font-medium uppercase">Region</span>
+                  <span>{profile.region}</span>
+                </div>
+                <div className="flex justify-between py-2">
+                  <span className="font-medium uppercase">School</span>
+                  <span>{profile.school_name}</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-white shadow-md rounded-2xl p-6 flex flex-col gap-4 hover:shadow-lg transition-shadow">
+              <div className="flex items-center gap-3 border-b pb-3">
+                <Book className="w-6 h-6 text-purple-600" />
+                <h2 className="text-lg font-semibold text-gray-800">
+                  Academic
+                </h2>
+              </div>
+              <div className="flex flex-col space-y-3 text-gray-700 text-sm">
+                <div className="flex justify-between py-2">
+                  <span className="font-medium uppercase">Grade</span>
+                  <span>{profile.grade}</span>
+                </div>
+                <div className="flex justify-between py-2">
+                  <span className="font-medium uppercase">Referral Source</span>
+                  <span>{profile.referral_source}</span>
                 </div>
               </div>
             </div>
